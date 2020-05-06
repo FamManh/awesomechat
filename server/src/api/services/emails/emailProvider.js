513af8dde7fe8c3fe2a5d028b9eb56585ddc63bb
fammanh <fammanh217@mail.ru>
2020-05-06 23:13:46 +0500

    fix send email

--- server/src/api/services/emails/emailProvider.js
+++ server/src/api/services/emails/emailProvider.js
@@ -1,21 +1,32 @@
 const nodemailer = require('nodemailer');
 const { emailConfig } = require('../../../config/vars');
 const Email = require('email-templates');
-
+const smtpTransport = require('nodemailer-smtp-transport')
 // SMTP is the main transport in Nodemailer for delivering messages.
 // SMTP is also the protocol used between almost all email hosts, so its truly universal.
 // if you dont want to use SMTP you can create your own transport here
 // such as an email service API or nodemailer-sendgrid-transport
 
-const transporter = nodemailer.createTransport({
-  port: emailConfig.port,
-  host: emailConfig.host,
-  auth: {
-    user: emailConfig.username,
-    pass: emailConfig.password,
-  }, 
-  secure: false, // upgrades later with STARTTLS -- change this based on the PORT
-});
+// const transporter = nodemailer.createTransport({
+//   port: emailConfig.port,
+//   host: emailConfig.host,
+//   auth: {
+//     user: emailConfig.username,
+//     pass: emailConfig.password,
+//   }, 
+//   secure: false, // upgrades later with STARTTLS -- change this based on the PORT
+// });
+
+const transporter = nodemailer.createTransport(
+  smtpTransport({
+    service: "gmail",
+    auth: {
+      user: emailConfig.username,
+      pass: emailConfig.password,
+    },
+    secure: false, // upgrades later with STARTTLS -- change this based on the PORT
+  })
+);
 
 // verify connection configuration
 transporter.verify((error) => {
